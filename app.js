74ee4bf10cb33a92ddd800364100655e57209480
Ritik k. <166933060+ritik-2a@users.noreply.github.com>
2025-11-02 15:41:10 +0530

    Refactor app.js for improved readability

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "./profile-server.env" });
 } // Load environment variables
-const cors = require("cors");
+//const cors = require("cors");
 require("dotenv").config();
 const express = require("express");
 const nodemailer = require("nodemailer");
@@ -66,5 +66,6 @@ app.post("/send-email", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
 
 
